7e41c42de791a88b9ca314ae4466c20486385611
No Name <43710608+rejeky2016@users.noreply.github.com>
2019-12-01 13:35:49 +0700

    Update download1.js

--- download1.js
+++ download1.js
@@ -1,5 +1,1 @@
-  this.playChanson = function(oEvent) {
-    var oItem = oEvent.currentTarget,
-      sDownload = oItem.getAttribute("data-download"),
-      sUrl = "https://docs.google.com/uc?export=open&id=" + sDownload;
-    /* A vous de gérer la manière de recupere l'ur du son */
+<a download="https://docs.google.com/uc?export=open&id=">
